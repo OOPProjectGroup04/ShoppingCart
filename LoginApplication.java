48163f646c23ab7e865ca52499daf3d9ee990f32
FreddyIngle <freddyingle@gmail.com>
2023-11-30 15:00:52 -0500

    updated product database and login committed

--- LoginApplication.java
+++ LoginApplication.java
@@ -1,92 +1,100 @@
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
-// LoginModel
-class LoginModel {
-    public boolean authenticate(String username, String password) {
-    
-        return "admin".equalsIgnoreCase(username) && "12345".equalsIgnoreCase(password);
+// UserModel (Model)
+class UserModel {
+    private List<ChangeListener> listeners = new ArrayList<>();
+    private boolean authenticated;
+
+    public void authenticate(String username, String password) {
+        boolean oldAuthenticated = this.authenticated;
+        this.authenticated = "admin".equalsIgnoreCase(username) && "12345".equalsIgnoreCase(password);
+        if (oldAuthenticated != this.authenticated) {
+            notifyListeners();
+        }
+    }
+
+    public boolean isAuthenticated() {
+        return authenticated;
+    }
+
+    public void addChangeListener(ChangeListener listener) {
+        listeners.add(listener);
+    }
+
+    private void notifyListeners() {
+        ChangeEvent event = new ChangeEvent(this);
+        for (ChangeListener listener : listeners) {
+            listener.stateChanged(event);
+        }
     }
 }
 
-// LoginView
+// LoginView (View)
 class LoginView extends JFrame {
-    JLabel userLabel = new JLabel("Username");
-    JTextField userTextField = new JTextField();
-    JLabel passwordLabel = new JLabel("Password");
-    JPasswordField passwordField = new JPasswordField();
-    JButton loginButton = new JButton("Login");
-    JButton resetButton = new JButton("Reset");
-    JLabel messageLabel = new JLabel();
-    JLabel titleLabel;
-    JRadioButton sellerRadioButton;
-    JRadioButton customerRadioButton;
-    ButtonGroup roleButtonGroup;
+    private JLabel userLabel = new JLabel("Username");
+    private JTextField userTextField = new JTextField();
+    private JLabel passwordLabel = new JLabel("Password");
+    private JPasswordField passwordField = new JPasswordField();
+    private JButton loginButton = new JButton("Login");
+    private JButton resetButton = new JButton("Reset");
+    private JLabel messageLabel = new JLabel();
+    private JLabel titleLabel = new JLabel("George's Shopping Cart App", SwingConstants.CENTER);
+    private JRadioButton sellerRadioButton = new JRadioButton("Seller");
+    private JRadioButton customerRadioButton = new JRadioButton("Customer");
+    private ButtonGroup roleButtonGroup = new ButtonGroup();
 
     public LoginView() {
-        
-        // Setup GUI components
-        // Set the frame properties
         setTitle("Login Form");
         setBounds(10, 10, 370, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
-        // Set the container and layout manager
+
         Container container = getContentPane();
         container.setLayout(null);
 
-        // Create and place the title label
-        titleLabel = new JLabel("George's Shopping Cart App", SwingConstants.CENTER);
         titleLabel.setFont(new Font("Serif", Font.BOLD, 20));
         titleLabel.setBounds(50, 30, 270, 30);
         container.add(titleLabel);
 
-        //Select either Seller or Customer
-        sellerRadioButton = new JRadioButton("Seller");
-        customerRadioButton = new JRadioButton("Customer");
         sellerRadioButton.setBounds(150, 270, 80, 30);
         customerRadioButton.setBounds(230, 270, 100, 30);
-        
-        // Group the radio buttons
-        roleButtonGroup = new ButtonGroup();
+
         roleButtonGroup.add(sellerRadioButton);
         roleButtonGroup.add(customerRadioButton);
-        
+
         container.add(sellerRadioButton);
         container.add(customerRadioButton);
-
-        // Preselect the "Customer" radio button
         customerRadioButton.setSelected(true);
 
-        // Create and place the username label
-        userLabel = new JLabel("Username");
         userLabel.setBounds(50, 150, 100, 30);
         container.add(userLabel);
 
-        // Create and place the username text field
-        userTextField = new JTextField();
         userTextField.setBounds(150, 150, 150, 30);
         container.add(userTextField);
 
-        // Create and place the password label
-        passwordLabel = new JLabel("Password");
         passwordLabel.setBounds(50, 220, 100, 30);
         container.add(passwordLabel);
 
-        // Create and place the password field
-        passwordField = new JPasswordField();
         passwordField.setBounds(150, 220, 150, 30);
         container.add(passwordField);
 
-        // Create and place the login button
-        loginButton = new JButton("Login");
         loginButton.setBounds(150, 300, 100, 30);
         container.add(loginButton);
+
+        resetButton.setBounds(150, 340, 100, 30);
+        container.add(resetButton);
+
+        messageLabel.setBounds(50, 380, 270, 30);
+        container.add(messageLabel);
     }
 
-    // Getters for the controller to access the view's components
     public String getUsername() {
         return userTextField.getText();
     }
@@ -99,49 +107,62 @@ class LoginView extends JFrame {
         return loginButton;
     }
 
+    public JButton getResetButton() {
+        return resetButton;
+    }
+
     public void displayErrorMessage(String errorMessage) {
         JOptionPane.showMessageDialog(this, errorMessage);
     }
-
-    
 
     public void displayMessage(String message, Color color) {
         messageLabel.setForeground(color);
         messageLabel.setText(message);
     }
+
+    public void attachController(ActionListener actionListener) {
+        loginButton.addActionListener(actionListener);
+        resetButton.addActionListener(actionListener);
+    }
+
+    public void updateLoginStatus(boolean isAuthenticated) {
+        String message = isAuthenticated ? "Login successful" : "Invalid username or password";
+        displayMessage(message, isAuthenticated ? Color.GREEN : Color.RED);
+    }
+    public void resetFields() {
+    userTextField.setText("");
+    passwordField.setText("");
 }
 
-// LoginController
-class LoginController implements ActionListener {
-    private LoginModel model;
+}
+
+// LoginController (Controller)
+class LoginController implements ActionListener, ChangeListener {
+    private UserModel model;
     private LoginView view;
 
-    public LoginController(LoginModel model, LoginView view) {
+    public LoginController(UserModel model, LoginView view) {
         this.model = model;
         this.view = view;
-        this.view.loginButton.addActionListener(this);
-        this.view.resetButton.addActionListener(this);
+        this.view.attachController(this);
+        this.model.addChangeListener(this);
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-        // Handle login action
-        if (e.getSource() == view.loginButton) {
-            String username = view.userTextField.getText();
-            String password = new String(view.passwordField.getPassword());
+public void actionPerformed(ActionEvent e) {
+    if (e.getSource() == view.getLoginButton()) {
+        String username = view.getUsername();
+        String password = view.getPassword();
+        model.authenticate(username, password);
+    } else if (e.getSource() == view.getResetButton()) {
+        view.resetFields();
+    }
+}
 
-            if (model.authenticate(username, password)) {
-                view.displayMessage("Login successful", Color.GREEN);
-            } else {
-                view.displayMessage("Invalid username or password", Color.RED);
-            }
-        }
 
-        // Handle reset action
-        if (e.getSource() == view.resetButton) {
-            view.userTextField.setText("");
-            view.passwordField.setText("");
-        }
+    @Override
+    public void stateChanged(ChangeEvent e) {
+        view.updateLoginStatus(model.isAuthenticated());
     }
 }
 
@@ -149,7 +170,7 @@ class LoginController implements ActionListener {
 public class LoginApplication {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            LoginModel model = new LoginModel();
+            UserModel model = new UserModel();
             LoginView view = new LoginView();
             new LoginController(model, view);
             view.setVisible(true);
--- Product.java
+++ Product.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Product {
     private int productID;
     private String name;
@@ -48,5 +51,32 @@ public class Product {
         this.quantity = newQuantity;
     }
 
+    // Static method to create and return a list of products
+    public static List<Product> getSampleProducts() {
+        List<Product> products = new ArrayList<>();
+        products.add(new Product(101, "Laptop", 999.99, 10, "path/to/laptop/image.jpg", "A high-performance laptop."));
+        products.add(new Product(102, "Smartphone", 499.99, 15, "path/to/smartphone/image.jpg", "An innovative smartphone with the best camera."));
+        products.add(new Product(103, "DashCam", 99.99, 30, "path/to/dashcam/image.jpg", "A Dashcam for your car."));
+        products.add(new Product(3, "Bluetooth Headphones", 89.99, 25, "path/to/headphones/image.jpg", "Bluetooth headphones with noise cancellation."));
+        products.add(new Product(4, "Electric Toothbrush", 39.99, 40, "path/to/toothbrush/image.jpg", "Rechargeable toothbrush with smart timer."));
+        products.add(new Product(5, "Gaming Laptop", 1299.99, 30, "path/to/console/image.jpg", "The latest gaming laptop with 24GB Ram and 1TB SSD."));
+        products.add(new Product(6, "Smart Watch", 199.99, 20, "path/to/watch/image.jpg", "A smartwatch with health and fitness tracking."));
+        products.add(new Product(7, "Tablet", 329.99, 15, "path/to/tablet/image.jpg", "A versatile tablet perfect for work and play."));
+        products.add(new Product(8, "Bluetooth Mouse", 24.99, 50, "path/to/mouse/image.jpg", "A comfortable mouse for everyday use."));
+        products.add(new Product(9, "E-Reader", 129.99, 30, "path/to/ereader/image.jpg", "An e-reader with a paper-like display."));
+        products.add(new Product(11, "Portable Speaker", 59.99, 35, "path/to/speaker/image.jpg", "A portable speaker with excellent sound quality."));
+        products.add(new Product(12, "Fitness Tracker", 49.99, 45, "path/to/tracker/image.jpg", "Track your daily activity and sleep."));
+        products.add(new Product(13, "Espresso Machine", 249.99, 15, "path/to/espresso/image.jpg", "Brew cafe-quality espresso at home with ease."));
+        products.add(new Product(14, "Yoga Mat", 19.99, 30, "path/to/yogamat/image.jpg", "Eco-friendly yoga mat with non-slip surface."));
+        products.add(new Product(15, "LED Desk Lamp", 45.99, 20, "path/to/desklamp/image.jpg", "Adjustable desk lamp with multiple brightness settings."));
+        products.add(new Product(17, "Cookware Set", 129.99, 25, "path/to/cookware/image.jpg", "Stainless steel cookware set for all your cooking needs."));
+        products.add(new Product(18, "Sunglasses", 89.99, 50, "path/to/sunglasses/image.jpg", "Stylish sunglasses with UV protection."));
+        products.add(new Product(19, "Backpack", 59.99, 35, "path/to/backpack/image.jpg", "Durable backpack for travel and everyday use."));
+        products.add(new Product(20, "Leather Wallet", 49.99, 45, "path/to/wallet/image.jpg", "Elegant leather wallet with RFID blocking."));
+        products.add(new Product(22, "Noise-cancelling Earplugs", 25.99, 50, "path/to/earplugs/image.jpg", "Reusable earplugs with superior noise cancellation."));
 
+        return products;
+    }
+
+List<Product> productList = Product.getSampleProducts();
 }
